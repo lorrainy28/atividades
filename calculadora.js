f3cd555bc6fe532b8f0027a8daae7b81c0a80fc3
lorrainy28 <lorrainyliviagasparino28@gmail.com>
2025-05-07 16:15:06 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+function somar(a ,b) {
+    var resultado= a + b;
+    console.log(" o reultado da soma : " + resultado)
+}
+
+function multiplicar (c ,d) {
+    var resultado= c * d;
+    console.log(" o resultado da multiplicacao :" +resultado)
+}
+module.exports= { somar,multiplicar };
